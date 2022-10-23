9ae87d30f0a9d061123bcc8f2f38b6905d3a937a
Jonathan <jonathan.arsola01@utrgv.edu>
2022-10-23 14:28:13 -0500

    working on having program click in the background.

--- seqClicker.py
+++ seqClicker.py
@@ -5,6 +5,10 @@
 
 
 import random, pyautogui, time, winsound
+from numpy import append
+import win32api, win32con, win32gui
+from pywinauto import Application
+import pywinauto as pwa
 
 inventoryCoord = []
 oreCoord = []
@@ -22,10 +26,12 @@ def clickPositions(cycles, locations):
             xRange = [locations[j][0], locations[j][2]]
             yRange = [locations[j][1], locations[j][3]]
             timeRange = [1, 2]    #range to sleep
-            preAction(xRange, yRange)
+            #preAction(xRange, yRange)
             x = random.randint(xRange[0],xRange[1])
             y = random.randint(yRange[0],yRange[1])
-            pyautogui.click(x,y)
+            #pyautogui.click(x,y)
+            hideLeftClick(x,y)
+            hideLeftClick(x,y)
             time.sleep(random.randint(timeRange[0],timeRange[1]))
         winsound.PlaySound("SystemExit", winsound.SND_ALIAS)
 
@@ -59,9 +65,23 @@ def getPositions(obj_count):
     print(f"Recieved Cordinates for {obj_count} objectives.")
     return allCoords
 
+def hideLeftClick(x,y):
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
+    time.sleep(.1)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
+    print('Left Click')
+
 
 
 if __name__ == "__main__":
+    app = Application().connect(process=13100)
+    time.sleep(2)
+    dlg = app.RuneLite
+    dialogs = app.windows()
+    temp = app.window(title_re=".*jinzo no.*")
+    pid = pwa.handleprops.processid(dialogs[0])
+    rect = pwa.handleprops.rectangle(dialogs[0])    #(L4, T5, R869, B1011)
+
     obj_count = int(input("How many objectives? "))
     locations = getPositions(obj_count)
     cycles = int(input("How many cycles? "))
