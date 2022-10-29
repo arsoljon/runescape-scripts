51bd8a4a8646dcb385c230acc8eb6b3f167243fc
Jonathan <jonathan.arsola01@utrgv.edu>
2022-10-28 21:23:36 -0500

    moved all path-finding related scripts into the path-finder directory. Created the outline of find-mine script

---
+++ path-finder/find_mine.py
@@ -0,0 +1,16 @@
+#Goal: using a snap shot of the minimap located on the top-right of runeLite.
+# locate the mining symbol. Then click said mining symbol to move towards it.
+
+#Locate the map
+#Locate the mine symbol
+#grab coordinates of mine symbol in map
+#click at said coordnates.
+
+def getMap():
+    pass
+
+def foundMine():
+    pass
+
+def clickMine():
+    pass
