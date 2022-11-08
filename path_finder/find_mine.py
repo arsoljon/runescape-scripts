d122917744f18cfbbaf9b3737dd95d1ecc136d3e
Jonathan <jonathan.arsola01@utrgv.edu>
2022-11-07 19:42:40 -0600

    in the examine picture method, I created a loop to look for the numpy array of the mining symbol picture within the numpy array of the map picture. still need to add comments for better clarification

--- path_finder/find_mine.py
+++ path_finder/find_mine.py
@@ -2,12 +2,14 @@
 # locate the mining symbol. Then click said mining symbol to move towards it.
 
 from sequential_tasks import seqClicker as sq
-import pyscreenshot, cv2, os, time
+import pyscreenshot, cv2, os, time, sys
 import numpy as np
 import matplotlib.pyplot as plt
 import cvlib as cv
 from cvlib.object_detection import draw_bbox
 from PIL import Image
+import pandas as pd
+np.set_printoptions(threshold=sys.maxsize)
 
 
 #Locate the map
@@ -47,30 +49,57 @@ def setMineSymbol():
     im = im.save("{}/{}".format(dirname, filename))
 
 def examinePicture():
-    name = ['mine-symbol.png', 'map.png']
+    name = ['mine-symbol.png', 'map.png', 'map1.png']
     cwd = os.getcwd()
     symbolPath = "{}\data\mine\{}".format(cwd,name[0])
     mapPath  = "{}\data\mine\{}".format(cwd,name[1])
-    print("Path : {}".format(symbolPath))
-    print("Path : {}".format(mapPath))
+    map1Path  = "{}\data\mine\{}".format(cwd,name[2])
     img = cv2.imread(symbolPath)
     #image = cv2.resize(img, (700,600))
-    cv2.imshow("Image", img)
-    cv2.waitKey(0)
+    #cv2.imshow("Image", img)
+    #cv2.waitKey(0)
     img = cv2.imread(mapPath)
     #image = cv2.resize(img, (700,600))
-    cv2.imshow("Image", img)
-    cv2.waitKey(0)
+    #cv2.imshow("Image", img)
+    #cv2.waitKey(0)
+    
     symImg = Image.open(symbolPath)
     mapImg = Image.open(mapPath)
+    map1Img = Image.open(map1Path)
     symArray = np.asarray(symImg)
     mapArray = np.asarray(mapImg)
-    print("Symbol : {} \n Map : {}".format(symArray, mapArray))
-    if (symArray == mapArray).all(1).any():
-        print("found")
-    else: 
-        print("not found")
+    map1Array = np.asarray(map1Img)
+    print("Symbol : {} \n Map : {}".format(symArray[0][0], symArray[0][0]))
+    print(np.array_equal(mapArray, map1Array))
+    print(np.where(pd.Index(pd.unique(symArray[11][0])).get_indexer(mapArray[0][0]) >= 0)[0])
 
+    newSet = []
+    for i in range(mapArray.shape[0]):
+        #default counter start points
+        mapIndex = i
+        symIndex = 0
+        found = 0
+        newSet = []
+        while(i < (mapArray.shape[0] - symArray.shape[0])):
+            if (symIndex >= symArray.shape[0]):
+                symIndex = 0
+                mapIndex += 1
+            unkonwnSetSize = np.where(pd.Index(pd.unique(symArray[symIndex][0])).get_indexer(mapArray[mapIndex][mapIndex+symIndex]) >= 0)[0].shape[0]
+            defaultSetSize = symArray.shape[2]
+            if(unkonwnSetSize == defaultSetSize):
+                found += 1
+                newSet.append(mapArray[mapIndex][mapIndex+symIndex])
+            if(unkonwnSetSize != defaultSetSize):
+                break
+            if(found == symArray.shape[0]):
+                print("found!!!!!")
+                break
+        if(found == symArray.shape[0]):
+            break
+
+    print(newSet)
+    print(symArray)
+                
     #cv2.imshow('image', fg)
     #plt.figure(figsize=(10,10))
 
